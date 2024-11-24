7eda9e23903e54998f60e3a345ca74217b8d2247
Dhiraj Prajapat <106270089+Dhiraj-Prajapat@users.noreply.github.com>
2024-11-24 13:32:45 +0530

    project uploaded

---
+++ src/pages/Hero.jsx
@@ -0,0 +1,115 @@
+import Marquee from "react-fast-marquee";
+const Hero = () => {
+  return (
+    <>
+      <div className="h-full w-full">
+        <div className="padding-top-header">
+          <div className="header-content-block">
+            <div className="subtitle-wrapper">
+              <Marquee>
+                Machine Learning&nbsp;–&nbsp;Computer Vision
+                Solutions&nbsp;–&nbsp;AI-Powered Analytics&nbsp;–&nbsp;AI
+                Consultation&nbsp;–&nbsp;
+              </Marquee>
+            </div>
+            <div className="heading-wrap">
+              <h1 className="heading-title">
+                Unlock the future <span className="featured-text">With ai</span>
+              </h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="h-1/3 w-full bg-transparent py-60"></div>
+
+        <div>
+          <div className="padding-section-medium">
+            <div className="text-size-regular text-align-center">
+              partners who trust us:
+            </div>
+            <div className="spacer-medium"></div>
+              <div className="partner-row-wrapper">
+                <div className="left-gradient"></div>
+                <marquee >
+                <div className="w-layout-grid partner-component-grid">
+                  <div
+                    id="w-node-ba484692-b5b8-26e5-8653-bcfb6d2ffe8e-803718fc"
+                    className="partner-item"
+                  >
+                    <img
+                      loading="lazy"
+                      src="https://cdn.prod.website-files.com/66d39c6cae70a65d79022708/66e44caf535cec7c1f8ba3ef_Springfield.svg"
+                      alt=""
+                      className="partner-logo"
+                    />
+                  </div>
+                  <div
+                    id="w-node-ba484692-b5b8-26e5-8653-bcfb6d2ffe90-803718fc"
+                    className="partner-item"
+                  >
+                    <img
+                      loading="lazy"
+                      src="https://cdn.prod.website-files.com/66d39c6cae70a65d79022708/66e44cae535cec7c1f8ba3e4_Delaware.svg"
+                      alt=""
+                      className="partner-logo"
+                    />
+                  </div>
+                  <div
+                    id="w-node-ba484692-b5b8-26e5-8653-bcfb6d2ffe92-803718fc"
+                    className="partner-item"
+                  >
+                    <img
+                      loading="lazy"
+                      src="https://cdn.prod.website-files.com/66d39c6cae70a65d79022708/66e44cae535cec7c1f8ba3e0_Sweden.svg"
+                      alt=""
+                      className="partner-logo"
+                    />
+                  </div>
+                  <div
+                    id="w-node-ba484692-b5b8-26e5-8653-bcfb6d2ffe94-803718fc"
+                    className="partner-item"
+                  >
+                    <img
+                      loading="lazy"
+                      src="https://cdn.prod.website-files.com/66d39c6cae70a65d79022708/66e44caf535cec7c1f8ba406_Umbrella.svg"
+                      alt=""
+                      className="partner-logo"
+                    />
+                  </div>
+                  <div
+                    id="w-node-ba484692-b5b8-26e5-8653-bcfb6d2ffe96-803718fc"
+                    className="partner-item"
+                  >
+                    <img
+                      loading="lazy"
+                      src="https://cdn.prod.website-files.com/66d39c6cae70a65d79022708/66e44cae535cec7c1f8ba3da_Monaco.svg"
+                      alt=""
+                      className="partner-logo"
+                    />
+                  </div>
+                  <div
+                    id="w-node-ba484692-b5b8-26e5-8653-bcfb6d2ffe98-803718fc"
+                    className="partner-item"
+                  >
+                    <img
+                      loading="lazy"
+                      src="https://cdn.prod.website-files.com/66d39c6cae70a65d79022708/66e44cae535cec7c1f8ba397_Amsterdam.svg"
+                      alt=""
+                      className="partner-logo"
+                    />
+                  </div>
+                </div>
+                
+                </marquee
+                <div className="right-gradient"></div>
+              </div>
+          </div>
+        </div>
+
+
+      </div>
+    </>
+  );
+};
+
+export default Hero;
